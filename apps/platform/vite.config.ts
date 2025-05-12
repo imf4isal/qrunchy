66a4e127760546d04d63b22a1d561b08e01a8a93
faisal <mdfaisal.dev@gmail.com>
2025-05-12 09:58:48 +0600

    solve docker-vite issue.

--- apps/platform/vite.config.ts
+++ apps/platform/vite.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: "0.0.0.0",
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
