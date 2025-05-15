c596512ff360a50fb47e27b989de8f31feed2f18
faisal <mdfaisal.dev@gmail.com>
2025-05-15 06:51:04 +0600

    update routing for photo menu creation

--- apps/platform/src/router/index.tsx
+++ apps/platform/src/router/index.tsx
@@ -1,12 +1,13 @@
 import { Route, Switch } from "wouter";
 import Home from "@/pages/home/Home";
 import NotFound from "@/pages/notFound/NotFound";
+import PhotoMenu from "@/pages/photomenu/PhotoMenu";
 
 export default function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/photo-menu">{() => <div>Photo Menu Page</div>}</Route>
+      <Route path="/photo-menu" component={PhotoMenu} />
       <Route path="/digital-menu">{() => <div>Digital Menu Page</div>}</Route>
       <Route component={NotFound} />
     </Switch>
